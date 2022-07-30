912963deaf6563cf10d19564b1d73565880c0b27
Soumajith <soumajithdas@gmail.com>
2022-07-30 19:05:41 +0530

    Changed print statement to log statement and linked all three views

--- lib/views/register_view.dart
+++ lib/views/register_view.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'dart:developer' as devtools show log;
 
 class RegisterView extends StatefulWidget {
   const RegisterView({Key? key}) : super(key: key);
@@ -61,13 +62,15 @@ class _RegisterViewState extends State<RegisterView> {
                 final userCredential = await FirebaseAuth.instance
                     .createUserWithEmailAndPassword(
                         email: email, password: password);
-                print(userCredential);
+                devtools.log(userCredential.toString());
               } on FirebaseAuthException catch (e) {
                 if (e.code == 'weak-password') {
-                  print("Weak Password! Retype");
+                  devtools.log("Weak Password! Retype");
                 } else if (e.code == 'email-already-in-use') {
-                  print("Email already registered!");
-                } else if (e.code == 'invalid-email') print("Invalid email");
+                  devtools.log("Email already registered!");
+                } else if (e.code == 'invalid-email') {
+                  devtools.log("Invalid email");
+                }
               }
             },
             child: const Text('Register'),
